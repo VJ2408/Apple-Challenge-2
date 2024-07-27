e9e172a5f45d739efc12785941d5c2a800655d0c
VJ2408 <vijai.adithya24@gmail.com>
2024-07-27 16:10:51 +0800

    Initial Commit

---
+++ Challenge 2/Challenge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_2App.swift
+//  Challenge 2
+//
+//  Created by Vijai Adithya on 27/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge 2
+//
+//  Created by Vijai Adithya on 27/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
