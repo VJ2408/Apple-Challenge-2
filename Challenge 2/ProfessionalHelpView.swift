17de69ad6673d7592dd9674c3a3ba55a9f2f9ca5
Qi Yang <hoqiyang2020@gmail.com>
2024-07-27 23:25:03 +0800

    Professional help tab. Not done, will continue when i have time

--- Challenge 2/ProfessionalHelpView.swift
+++ Challenge 2/ProfessionalHelpView.swift
@@ -9,10 +9,27 @@ import SwiftUI
 
 struct ProfessionalHelpView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                Text("Help Hotlines")
+                    .bold()
+                    .padding(20)
+                Link(destination: URL(string: "https://www.gov.sg/article/call-these-helplines-if-you-need-emotional-or-psychological-support")!) {
+                    Image(systemName: "link.circle.fill")
+                        .font(.largeTitle)
+                    Spacer()
+                }
+            }
+            HStack{
+                Text("Resources for mental well-being")
+                    .bold()
+                    .padding(20)
+                Link(destination: URL(string: "https://www.ncss.gov.sg/our-initiatives/beyond-the-label/resources")!) {
+                    Image(systemName: "link.circle.fill")
+                        .font(.largeTitle)
+                    Spacer()
+                }
+            }
+        }
     }
-}
-
-#Preview {
-    ProfessionalHelpView()
 }
