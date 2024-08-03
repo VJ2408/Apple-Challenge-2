ff1c779f5453e952214a455df4ba56730a1fac64
Qi Yang <hoqiyang2024@gmail.com>
2024-08-03 15:24:10 +0800

    YAY

--- Challenge 2/HomeView.swift
+++ Challenge 2/HomeView.swift
@@ -6,12 +6,36 @@
 //
 
 import SwiftUI
-
 struct HomeView: View {
+    @State private var progress = 0.7
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            ZStack {
+                Circle()
+                    .stroke(
+                        Color.pink.opacity(0.5),
+                        lineWidth: 20
+                    )
+                Circle()
+                    .trim(from: 0, to: progress)
+                    .stroke(
+                        Color.pink,
+                        style: StrokeStyle(
+                            lineWidth: 20,
+                            lineCap: .round
+                        )
+                    )
+                    .rotationEffect(.degrees(-90))
+                // 1
+                    .animation(.easeOut, value: progress)
+                
+            }
+            .padding(30 )
+            
+        }
     }
 }
+
 
 #Preview {
     HomeView()
--- Challenge 2/ProfessionalHelpView.swift
+++ Challenge 2/ProfessionalHelpView.swift
@@ -9,27 +9,36 @@ import SwiftUI
 
 struct ProfessionalHelpView: View {
     var body: some View {
-        VStack{
-            HStack{
-                Text("Help Hotlines")
-                    .bold()
-                    .padding(20)
-                Link(destination: URL(string: "https://www.gov.sg/article/call-these-helplines-if-you-need-emotional-or-psychological-support")!) {
-                    Image(systemName: "link.circle.fill")
-                        .font(.largeTitle)
-                    Spacer()
+        NavigationView{
+            VStack{
+                HStack{
+                    Text("Help Hotlines")
+                        .bold()
+                        .padding(20)
+                    Link(destination: URL(string: "https://www.gov.sg/article/call-these-helplines-if-you-need-emotional-or-psychological-support")!) {
+                        Image(systemName: "link.circle.fill")
+                            .font(.largeTitle)
+                        Spacer()
+                    }
                 }
-            }
-            HStack{
-                Text("Resources for mental well-being")
-                    .bold()
-                    .padding(20)
-                Link(destination: URL(string: "https://www.ncss.gov.sg/our-initiatives/beyond-the-label/resources")!) {
-                    Image(systemName: "link.circle.fill")
-                        .font(.largeTitle)
-                    Spacer()
+                HStack{
+                    Text("Resources for mental well-being")
+                        .bold()
+                        .padding(20)
+                    Link(destination: URL(string: "https://www.ncss.gov.sg/our-initiatives/beyond-the-label/resources")!) {
+                        Image(systemName: "link.circle.fill")
+                            .font(.largeTitle)
+                        Spacer()
+                    }
                 }
+                Spacer()
             }
+            .navigationTitle("Professional Help")
         }
     }
+}
+
+
+#Preview {
+    ContentView()
 }
