221e6f5ea8b532ba0ecdefc814eb1b9f69c855af
Qi Yang <hoqiyang2024@gmail.com>
2024-08-10 16:22:43 +0800

    Home Page nearly done [Im tired]

--- Challenge 2/HomeView.swift
+++ Challenge 2/HomeView.swift
@@ -1,13 +1,10 @@
-//
-//  HomeView.swift
-//  Challenge 2
-//
-//  Created by T Krobot on 27/7/24.
-//
+
 
 import SwiftUI
 struct HomeView: View {
-    @State private var progress = 0.7
+    let daysInMonth = 31
+    let streakDays: Set<Int> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    @State private var progress = 0.3
     var body: some View {
         VStack{
             ZStack {
@@ -26,12 +23,46 @@ struct HomeView: View {
                         )
                     )
                     .rotationEffect(.degrees(-90))
-                // 1
                     .animation(.easeOut, value: progress)
-                
+                VStack{
+                    Text("XP")
+                        .bold()
+                        .font(.system(size: 30))
+                    Text(" 10000")
+                        .font(.system(size: 20))
+                    Text("/30000")
+                        .font(.system(size: 20))
+                        
+                }
             }
             .padding(30 )
-            
+            VStack {
+                HStack {
+                    ForEach(["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"], id: \.self) { day in
+                        Text(day)
+                            .frame(maxWidth: .infinity)
+                    }
+                }
+                let columns = Array(repeating: GridItem(.flexible(), spacing: 10), count: 7)
+                LazyVGrid(columns: columns, spacing: 10) {
+                    ForEach(1...daysInMonth, id: \.self) { day in
+                        Text("\(day)")
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .background(streakDays.contains(day) ? Color.blue : Color.clear)
+                            .foregroundColor(streakDays.contains(day) ? Color.white : Color.black)
+                            .clipShape(RoundedRectangle(cornerRadius: 30))
+                            .padding(8)
+                    }
+                }
+                .padding()
+            }
+            .padding(22)
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
         }
     }
 }
